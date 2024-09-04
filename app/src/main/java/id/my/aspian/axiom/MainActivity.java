a0ca3890da76393fb4817ac2432c63b7c432b22f
Aspian <p.aspian1738@gmail.com>
2024-09-05 07:02:49 +0800

    calculate

--- app/src/main/java/id/my/aspian/axiom/MainActivity.java
+++ app/src/main/java/id/my/aspian/axiom/MainActivity.java
@@ -1,7 +1,10 @@
 package id.my.aspian.axiom;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,9 +25,27 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        findViewById(R.id.about).setOnClickListener(v -> {
-            Intent intent = new Intent(this, AboutActivity.class);
-            startActivity(intent);
-        });
+//        findViewById(R.id.about).setOnClickListener(v -> {});
+    }
+
+    @SuppressLint("DefaultLocale")
+    public void calculate(View v) {
+        float result = 0;
+        EditText[] mata_pelajaran = {
+            findViewById(R.id.bahasa_indonesia),
+            findViewById(R.id.bahasa_inggris),
+            findViewById(R.id.matematika),
+            findViewById(R.id.sejarah),
+            findViewById(R.id.ppkn),
+            findViewById(R.id.pai)
+        };
+
+        for (EditText mapel : mata_pelajaran) {
+            String raw = mapel.getText().toString();
+            result += (raw.isEmpty()) ? 0 : Float.parseFloat(raw);
+        }
+
+        Button button = findViewById(R.id.button);
+        button.setText(String.format("%.2f", result / 6));
     }
 }
--- app/src/main/java/id/my/aspian/axiom/OutputActivity.java
+++ app/src/main/java/id/my/aspian/axiom/OutputActivity.java
@@ -1,6 +1,7 @@
 package id.my.aspian.axiom;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,8 @@ public class OutputActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView obj = findViewById(R.id.nama);
+        obj.setText("Nama");
     }
 }
